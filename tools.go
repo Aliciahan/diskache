1a42edc589bbfd0f3d549a9892bbe084b13d3eca
Alicia HAN <alicia.han007@gmail.com>
2020-01-21 16:10:12 +0100

    :syringe: fix

---
+++ tools.go
@@ -0,0 +1,26 @@
+package diskache
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+func GetBytesFromStruct(data interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(data)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func GetInterfaceFromBytes(bts []byte, data interface{})error {
+	buf := bytes.NewBuffer(bts)
+	dec := gob.NewDecoder(buf)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
